2a95469887d5e57bddfa22476157a1fcf269b71a
Jherald Vibar <vibar_jherald@spcc.edu.ph>
2025-06-02 16:17:53 +0800

    updated

--- resources/views/voyair_airlines_landing-page.blade.php
+++ resources/views/voyair_airlines_landing-page.blade.php
@@ -389,5 +389,31 @@
         </div>
     </div>
     </div>
+    <footer class="bg-blue-900 rounded-lg shadow-sm text-white">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="{{asset('web_images/image 1.png')}}" class="h-8" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap">{{config('app.name')}}</span>
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-blue-800 sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-white sm:text-center">&copy; 2025 <a href="{{asset('web_images/image 1.png')}}" class="hover:underline">{{config('app.name')}}</a>. All Rights Reserved.</span>
+    </div>
+</footer>
 </body>
 </html>
